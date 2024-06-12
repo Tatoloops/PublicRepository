7796d3cde97e5dd4cb7cfe2305de60e148a07325
Tom C <matatopotato@gmail.com>
2024-06-12 18:49:12 -0400

    -Poke-Trainer CPC Updated:  -Program can now Place all CPCs needed in the most efficient way  -Added more request files to test

--- Python/Poke-Trainer CPC/Core/Search.py
+++ Python/Poke-Trainer CPC/Core/Search.py
@@ -17,15 +17,29 @@ def instalarCpc(grafo):
 	# initialize
 	initializeSpecialAttributes(grafo)
 	edgeList= []
+
+
 	# instala los CPC en aquellos nodos que solo tienen 1 posible CPC
-	noMoreEmergencyCpcs=False
-	while noMoreEmergencyCpcs==False:
-		noMoreEmergencyCpcs=True
+	priority=1
+	noMoreCpcs=False
+	noMoreCurrentCpcs=False
+	while noMoreCpcs==False:
+		#despues de analizar todos los nodos, si se agrego si quiera 1 solo CPC
+		# entonces se vuelve a iterar TODOS los nodos, hasta que itere por TODOS los nodos y
+
+		if noMoreCurrentCpcs==True:
+			priority+=1
+
+		noMoreCpcs=True
+		noMoreCurrentCpcs=True
+		#vuelve a analizar todos los nodos
 		for n in grafo.nodes:
 			
 			#si el nodo no tiene una ruta con centro pokemon
 			if grafo.nodes[n]['hasCPC']==False:
 
+				noMoreCpcs=False
+				noMoreCurrentCpcs=False
 				#se calcula el nivel de prioridad
 				emergencyLevel = grafo.degree[n]
 
@@ -37,9 +51,23 @@ def instalarCpc(grafo):
 				# 	if grafo.edges[(n, neighbor)]['hasCPC']:
 				# 		emergencyLevel -= 1
 
+				#instalando CPC dependiendo del nivel
+				if emergencyLevel==0:
+
+					for neighbor in grafo.neighbors(n):
+						# Modificar 'hasCPC' value to True on both nodes
+						grafo.nodes[n]['hasCPC'] = True
+						grafo.nodes[neighbor]['hasCPC'] = True
+
+						# Modificar 'hasCPC' value on the edge
+						grafo.edges[(n, neighbor)]['hasCPC'] = True
+						showCpcPlacement(n,neighbor)
+						priority=1
+						break
+
 				#si solo tiene un posible edge para CPC lo instala ahi
-				if emergencyLevel==1:
-					noMoreEmergencyCpcs=False
+				elif emergencyLevel==priority:
+
 					for neighbor in grafo.neighbors(n):
 						if 'hasCPC' not in grafo.nodes[neighbor] or grafo.nodes[neighbor]['hasCPC'] == False:
 							# Modificar 'hasCPC' value to True on both nodes
@@ -51,16 +79,8 @@ def instalarCpc(grafo):
 							showCpcPlacement(n,neighbor)
 							#for attribute, value in grafo.nodes[n].items():
 							#	print(attribute, ":", value)
+							priority=1
 							break  # No need to check other neighbors if CPC is installed
-				elif emergencyLevel==0:
-					noMoreEmergencyCpcs=False
-					for neighbor in grafo.neighbors(n):
-						# Modificar 'hasCPC' value to True on both nodes
-						grafo.nodes[n]['hasCPC'] = True
-						grafo.nodes[neighbor]['hasCPC'] = True
-
-						# Modificar 'hasCPC' value on the edge
-						grafo.edges[(n, neighbor)]['hasCPC'] = True
-						showCpcPlacement(n,neighbor)
-						break
+				else:
+					noMoreCurrentCpcs=True
 	return edgeList
