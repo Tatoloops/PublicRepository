c87f74691a82f5de12f7c931fd37adc80cd44c21
Tom C <matatopotato@gmail.com>
2024-08-11 14:31:13 -0400

    added first app downloader bash script

---
+++ School/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+URL="https://github.com/niXman/mingw-builds-binaries/releases/download/13.2.0-rt_v11-rev0/x86_64-13.2.0-release-posix-seh-ucrt-rt_v11-rev0.7z"
+OUTPUT="mingw64.zip"
+
+wget -O "$OUTPUT" "$URL"
