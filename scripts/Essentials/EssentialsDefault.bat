00b697e0db3d881caccf4b25810bf75ae40c126d
Tomás <matatopotato@gmail.com>
2024-10-20 11:27:59 -0300

    create Essentials script #1

---
+++ scripts/Essentials/EssentialsDefault.bat
@@ -0,0 +1,35 @@
+@echo off
+
+:: --- Retrieve Path ---
+REM Check if an argument is provided
+if "%~1"=="" (
+    REM If no argument is provided, set the default value
+    set folderName=Librarium
+) else (
+    REM If an argument is provided, use it
+    set folderName=%~1
+)
+
+if "%~2"=="" (
+    REM If no argument is provided, set the default value
+    set folderName=Librarium
+) else (
+    REM If an argument is provided, use it
+    set folderName=%~1
+)
+
+:: --- Set up Directory ---
+
+:: - Create the new folder in the user's folder
+mkdir "%USERPROFILE%\%folderName%"
+set myPath="%USERPROFILE%\%folderName%"
+
+
+:: --- Download Essentials ---
+REM 1. download mingw64 and add to path
+call "%myPath%\PublicRepository-master\scripts\dependencies\mingw64\setupAuto.bat" %myPath% 
+
+rem :: 2. install sublime text editor
+
+rem :: 3. install git
+
---
+++ scripts/Essentials/oneSetup/oneSetup.bat
@@ -0,0 +1,23 @@
+@echo off
+
+:: --- Create Librarium Folder ---
+set folderName=Librarium
+mkdir "%USERPROFILE%\%folderName%"
+
+:: --- Set variables ---
+set myPath=%USERPROFILE%\%folderName%
+set myRepo=https://github.com/Tatoloops/PublicRepository/archive/refs/heads/master.zip
+set fileToExtract=%myPath%\PublicRepository.zip
+
+:: --- Download repository ---
+curl -L -o "%fileToExtract%" %myRepo%
+
+:: --- Extract repository ---
+powershell -command "Expand-Archive -Path '%fileToExtract%' -DestinationPath '%myPath%' -Force"
+
+:: --- Execute main script ---
+call "%myPath%\PublicRepository-master\scripts\Essentials\EssentialsDefault.bat" %folderName%
+
+:: --- Clean up ---
+del "%fileToExtract%"
+rd /s /q "%myPath%\PublicRepository-master\"
