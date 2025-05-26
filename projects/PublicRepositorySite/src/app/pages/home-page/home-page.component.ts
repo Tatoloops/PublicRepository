1be0ee2796cc87374cae9cea09dd63eff6d32f9f
Tatoloops <matatopotato@gmail.com>
2025-05-26 08:10:36 -0400

    PublicRepositorySite Update

--- projects/PublicRepositorySite/src/app/pages/home-page/home-page.component.ts
+++ projects/PublicRepositorySite/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,44 @@
+// angular
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// angular material
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCardModule, 
+    MatButtonModule, 
+    MatIconModule
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
-
+  sections = [
+    {
+      title: 'Sección 1',
+      description: 'Explora la primera sección de nuestra aplicación.',
+      image: 'assets/img/section1.jpg',
+      link: '/seccion1'
+    },
+    {
+      title: 'Sección 2',
+      description: 'Descubre más sobre este apartado interesante.',
+      image: 'assets/img/section2.jpg',
+      link: '/seccion2'
+    },
+    {
+      title: 'Sección 3',
+      description: 'Visita esta sección especial de la aplicación.',
+      image: 'assets/img/section3.jpg',
+      link: '/seccion3'
+    }
+  ];
 }
--- projects/PublicRepositorySite/src/app/shared/components/navigation-comp/navigation-comp.component.ts
+++ projects/PublicRepositorySite/src/app/shared/components/navigation-comp/navigation-comp.component.ts
@@ -3,17 +3,26 @@ import { Component } from '@angular/core';
 import { Router,ActivatedRoute } from '@angular/router';
 
 //angular material
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-navigation-comp',
-  imports: [MatIconModule],
+  imports: [CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule
+  ],
   templateUrl: './navigation-comp.component.html',
   styleUrl: './navigation-comp.component.scss'
 })
 export class NavigationCompComponent {
   constructor(private router: Router, private route: ActivatedRoute) { }
 
+
+  //navigation methods
   navigateToHome() {
     this.router.navigate(['/home']);
   }
@@ -26,4 +35,15 @@ export class NavigationCompComponent {
   navigateToProjects() {
     this.router.navigate(['/projects']);
   }
+
+  // theme
+  currentTheme = 'theme-ultraviolet-default';
+  
+  setTheme(themeClass: string) {
+    this.currentTheme = themeClass;
+    document.body.className = '';
+    document.body.classList.add(themeClass);
+  }
+
+
 }
